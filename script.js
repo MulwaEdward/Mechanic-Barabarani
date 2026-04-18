ed6cfeacb4ae76aa86615d703763c560fefcdda6
Mulwa <mulwaeddy@gmail.com>
2026-04-18 12:09:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ //JSON data
+ const mechanics = [
+    { id: 1, name: "Nairobi Garage Motors", town: "Nairoboi", phone: "0711159499", whatsapp: "0799900000", spec: "Brakes & Suspension", available: true, rating: 4},
+    { id: 2, name: "Mombasa Quick Fix", town: "Mombasa", phone: "0712345679", whatsapp: "0123456790", spec: "Engine", available: true, rating: 5 },
+    { id: 3, name: "Kisumu Roadside", town: "Kisumu", phone: "01122334455", whatsapp: "01122334455", spec: "General", available: false, rating: 3}
+ ];
+
+ //Map & searching
